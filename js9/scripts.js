f8cfa734d1692a661b53cfee849c538c9022e2f3
Christian Alves <122300607+Christianlves@users.noreply.github.com>
2023-04-09 12:18:45 -0300

    Add files via upload

---
+++ js9/scripts.js
@@ -0,0 +1,11 @@
+//Notas
+let nota1 = parseFloat(prompt("Informe a nota 1"));
+let nota2 = parseFloat(prompt("Informe a nota 2"));
+let nota3 = parseFloat(prompt("Informe a nota 3"));
+let nota4 = parseFloat(prompt("Informe a nota 4"));
+
+//Calcular média 
+let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+//Exibir a nota na tela
+console.log(alert("Media: " + media.toFixed(2)));
